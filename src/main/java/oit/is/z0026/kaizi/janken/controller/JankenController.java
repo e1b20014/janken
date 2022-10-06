12a71de062a73e5f09a7ad63de8e17ce2b087544
e1b20014 <e1b20014@oit.ac.jp>
2022-10-06 13:28:13 +0900

    じゃんけんの手を選んでCPUと対戦する処理を実装する

--- src/main/java/oit/is/z0026/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0026/kaizi/janken/controller/JankenController.java
@@ -3,20 +3,39 @@ package oit.is.z0026.kaizi.janken.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class JankenController {
-    @GetMapping("/janken")
+    @GetMapping("/JankenController")
     public String janken() {
       return "janken.html";
     }
 
-    @PostMapping("/janken")
+    @PostMapping("/JankenController")
     public String janken(@RequestParam String user, ModelMap model) {
-      model.addAttribute("username",user); //userがもってきた変数
+      model.addAttribute("username","Hi! "+user); //userがもってきた変数
+      return "janken.html";
+    }
+
+    @GetMapping("/Janken")
+    public String param(@RequestParam String param1, ModelMap model) {
+      model.addAttribute("parameter","自分の手 "+param1); //paramがもってきた変数
+      model.addAttribute("cpu","相手の手 グー");
+      String gu = "グー";
+      String win = "勝ちです";
+      String lose = "負けです";
+      String draw = "あいこです";
+      String pa = "パー";
+      String tyo = "チョキ";
+      if(param1.equals(gu)){
+        model.addAttribute("compare","結果 "+draw);
+      }else if(param1.equals(pa)){
+        model.addAttribute("compare","結果 "+win);
+      }else if(param1.equals(tyo)){
+        model.addAttribute("compare","結果 "+lose);
+      }
       return "janken.html";
     }
 }
---
+++ src/main/java/oit/is/z0026/kaizi/janken/mode/Janken.java
@@ -0,0 +1,17 @@
+/*package oit.is.z0026.kaizi.janken.mode;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class Janken {
+    @GetMapping("/Janken") //jankenに直接参照できるようにする
+    public String janken() {
+      return "janken.html";
+    }
+
+
+
+}*/
