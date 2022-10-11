afbd6827d5f39fd6ecbcbed1569bff2020c8614e
e1b20014 <e1b20014@oit.ac.jp>
2022-10-11 16:37:39 +0900

    http://localhost:8080/ にアクセスすると↑に示すhtmlが表示される

--- src/main/java/oit/is/z0026/kaizi/janken/controller/JankenController.java
+++ src/main/java/oit/is/z0026/kaizi/janken/controller/JankenController.java
@@ -3,7 +3,7 @@ package oit.is.z0026.kaizi.janken.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
